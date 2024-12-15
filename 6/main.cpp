cb4623fb4c0a80b1f4a3e6546d3d86ba38fae5ab
andrey2005q <shuleha14@gmail.com>
2024-12-15 23:50:52 +0200

    main.cpp

--- 6/main.cpp
+++ 6/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "TeamManager.h"
 
+int main() {
+    TeamManager manager;
+
+    // Add teams
+    manager.addTeam("Dynamo", "Kyiv", 1927, "Mircea Lucescu");
+    manager.addTeam("Shakhtar", "Donetsk", 1936, "Igor Jovicevic");
+    manager.addTeam("Karpaty", "Lviv", 1963, "Andriy Tlumak");
+
+    // Display all teams
+    std::cout << "\nAll Teams:\n";
+    manager.displayTeams();
+
+    // Display teams founded after a specific year
+    int year;
+    std::cout << "\nEnter a year to find teams founded after that year: ";
+    std::cin >> year;
+    manager.displayTeamsFoundedAfter(year);
+
+    return 0;
