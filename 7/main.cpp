612978c8b5a5ff62fdf7bafb7895186abdb32385
andrey2005q <shuleha14@gmail.com>
2024-12-16 00:04:05 +0200

    Update main.cpp

--- 7/main.cpp
+++ 7/main.cpp
@@ -1,1 +1,20 @@
+// main.cpp
+#include "Student.h"
 
+int main() {
+    Student studentManager;
+
+    studentManager.addStudent(1, "Alice", 2);
+    studentManager.addStudent(2, "Bob", 3);
+
+    std::cout << "Initial list of students:" << std::endl;
+    studentManager.displayStudents();
+
+    studentManager.updateStudent(1, "Alice Smith", 4);
+    studentManager.removeStudent(2);
+
+    std::cout << "Final list of students:" << std::endl;
+    studentManager.displayStudents();
+
+    return 0;
+}
